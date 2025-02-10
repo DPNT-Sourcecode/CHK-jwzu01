a5e1be92a979222312de4e53792a63bd2e897013
sbx_user1051 <sbx_user1051@169.254.5.205>
2025-02-10 19:10:42 +0000

    Mon Feb 10 19:10:42 UTC 2025

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -2,6 +2,20 @@ package solutions.CHK
 
 object CheckoutSolution {
     fun checkout(skus: String): Int {
-        skus.count( i)
+        skus.groupingBy { it }.eachCount().let {
+            return it.calculateCartTotal()
+        }
+    }
+
+    private fun Map<Char, Int>.calculateCartTotal(): Int{
+        return this.entries.sumOf { (item, quantity) ->
+            when (item) {
+                'A' -> quantity / 3 * 130 + quantity % 3 * 50
+                'B' -> quantity / 2 * 45 + quantity % 2 * 30
+                'C' -> quantity * 20
+                'D' -> quantity * 15
+                else -> throw IllegalArgumentException("Invalid SKU")
+            }
+        }
     }
 }
