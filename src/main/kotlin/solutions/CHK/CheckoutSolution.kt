21c0ec13dbec6d8078e3a04b1666403486b8da8c
sbx_user1051 <sbx_user1051@169.254.5.205>
2025-02-10 19:31:33 +0000

    Mon Feb 10 19:31:33 UTC 2025

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -1,29 +1,35 @@
 package solutions.CHK
 
-import java.util.*
-
 object CheckoutSolution {
     fun checkout(skus: String): Int {
         skus.groupingBy { it }.eachCount().let {
-            return it.calculateCartTotal()
+            return it.removeFreeItems().calculateCartTotal()
         }
     }
 
     private fun Map<Char, Int>.calculateCartTotal(): Int {
-        val consolidationItemsMap = this.toMutableMap()
-        consolidationItemsMap['E']?.let {
-            consolidationItemsMap['B'] = consolidationItemsMap['B']?.minus(consolidationItemsMap['E']!!.div(2)) ?: 0
-        }
-        return consolidationItemsMap.entries.sumOf { (item, quantity) ->
+        return this.entries.sumOf { (item, quantity) ->
             when (item) {
                 'A' -> quantity / 5 * 200 + (quantity % 5) / 3 * 130 + (quantity % 5) % 3 * 50
                 'B' -> quantity / 2 * 45 + quantity % 2 * 30
                 'C' -> quantity * 20
                 'D' -> quantity * 15
                 'E' -> quantity * 40
+                'F' -> quantity * 10
                 else -> return -1
             }
         }
+    }
+
+    private fun Map<Char, Int>.removeFreeItems(): Map<Char, Int>{
+        val consolidationItemsMap = this.toMutableMap()
+        consolidationItemsMap['E']?.let {
+            consolidationItemsMap['B'] = consolidationItemsMap['B']?.minus(consolidationItemsMap['E']!!.div(2)) ?: 0
+        }
+        consolidationItemsMap['F']?.let {
+            consolidationItemsMap['F'] = consolidationItemsMap['F']?.minus(consolidationItemsMap['F']!!.div(3)) ?: 0
+        }
+        return consolidationItemsMap
     }
 }
 
